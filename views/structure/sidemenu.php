4bb646be7492522afd02364cb87eb4b2f1522ba7
ddiola24 <dianneyvonnemanalang@gmail.com>
2019-01-31 15:20:24 +0800

    logo

--- views/structure/sidemenu.php
+++ views/structure/sidemenu.php
@@ -1,7 +1,7 @@
 <div class="col-md-3 left_col">
           <div class="left_col scroll-view">
             <div class="navbar nav_title" style="border: 0;">
-              <a href="home.php" class="site_title"><i class="fa fa-paw"></i> <span>Lend Web!</span></a>
+              <a href="home.php" class="site_title"><img src="../vendors/img/favicon.png" width="50px" height="50px">   Lend Web!</span></a>
             </div>
 
             <div class="clearfix"></div>
